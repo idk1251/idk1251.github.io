dc3835254e0ed6e49a4e4784da554a9051f2c50e
idk1251 <143218303+idk1251@users.noreply.github.com>
2024-07-09 17:35:30 +0200

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsConfigPaths()]
-})
