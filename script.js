a8176c5ad760ee2b14610ac71fe4df742a72ed45
idk1251 <143218303+idk1251@users.noreply.github.com>
2024-07-09 17:02:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-document.querySelector('.bet-button').addEventListener('click', function() {
-    let dice = document.getElementById('dice');
-    dice.style.transform = 'rotate(' + (Math.floor(Math.random() * 360)) + 'deg)';
-});
+
