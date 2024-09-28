6ea77a6f69b941b07fb533d6615679a8d4db3b17
idk1251 <143218303+idk1251@users.noreply.github.com>
2024-09-28 20:10:04 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,17 +1,3 @@
-// background.js
 chrome.runtime.onInstalled.addListener(() => {
-  // Default values
-  chrome.storage.local.set({ features: { lightText: false } });
-});
-
-// Listen for updates
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'getFeatures') {
-    chrome.storage.local.get('features', (data) => {
-      sendResponse(data.features);
-    });
-    return true; // Indicates async response
-  } else if (request.action === 'setFeatures') {
-    chrome.storage.local.set({ features: request.features });
-  }
+  console.log('Extension installed and ready to use.');
 });
